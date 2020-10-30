9b0f4f99b15011f79fa6659c5043ad304c57d272
Bryan Ramirez <b.rq@me.com>
2020-10-29 22:12:09 -0600

    feat(report-module): add report transformation for serialization

--- src/modules/report/report.controller.ts
+++ src/modules/report/report.controller.ts
@@ -1,13 +1,17 @@
 import {
   Body,
+  ClassSerializerInterceptor,
   Controller,
   Post,
+  UseInterceptors,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { from, Observable } from 'rxjs';
+import { switchMap, toArray, map } from 'rxjs/operators';
 import { ReportDto } from './report.dto';
 import { ReportService } from './report.service';
+import { Report } from './report.transformer';
 import { IPReport } from './report.types';
 
 @Controller()
@@ -15,12 +19,19 @@ export class ReportController {
   constructor(private reportService: ReportService) {}
 
   // RPC (Remote Procedure Call) is prefered over REST due we are calling a procedure instead of a entity maintenance (CRUD)
+  @UseInterceptors(ClassSerializerInterceptor)
   @Post('generateMultipleIpReport')
   @UsePipes(new ValidationPipe({ transform: true }))
-  generateMultipleIpReport(@Body() report: ReportDto): Observable<IPReport[]> {
-    return this.reportService.generateMultipleReport(
-      report.ip,
-      report.reportServices,
-    );
+  generateMultipleIpReport(@Body() report: ReportDto): Observable<Report[]> {
+    return this.reportService
+      .generateMultipleReport(report.ip, report.reportServices)
+      .pipe(
+        switchMap(ipReports =>
+          from(ipReports).pipe(
+            map(ipReport => new Report(ipReport)),
+            toArray(),
+          ),
+        ),
+      );
   }
 }
